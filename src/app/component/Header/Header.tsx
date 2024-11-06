fde60dd8b175801404eecf32152271d12b435b7e
[Haris Arif] <[Ha70270217@email.com]>
2024-11-06 12:24:30 +0500

    first commit

---
+++ src/app/component/Header/Header.tsx
@@ -0,0 +1,11 @@
+function Header (){
+    return(
+            <div className=" bg-yellow-600 font-serif font-[400] leading-[93.75px]  h-[94px] 
+            top-[20px] left[36px] font-extrabold text-gray-100 text-3xl pl-[36px]">
+                MANZZARI
+            </div>
+
+    )
+}
+
+export default Header;
